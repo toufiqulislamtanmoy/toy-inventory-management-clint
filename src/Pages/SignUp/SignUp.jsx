2572724bb9645b85ebdec726d573c29e48e26892
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-05-18 11:04:41 +0600

    signup route style done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import login from "../../assets/login/login.json"
 import Lottie from "lottie-react"
-const Login = () => {
+const SignUp = () => {
     return (
         <div className="hero min-h-screen ">
             <div className="hero-content flex-col lg:flex-row-reverse border border-red-600 rounded-2xl">
@@ -10,13 +10,25 @@ const Login = () => {
                     <Lottie animationData={login}></Lottie>
                 </div>
                 <div className="card-body w-full rounded-3xl ">
-                    <div className="divider text-2xl font-bold">Login</div>
+                    <div className="divider text-2xl font-bold">Sign Up</div>
                     <form>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name="photoUrl" placeholder="Photo URL" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -28,19 +40,15 @@ const Login = () => {
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Sign Up</button>
                         </div>
 
-                        <p className="mt-3 space-x-2">Don't Have any account? <Link className="text-blue-600 underline" to="/signup">Sign up</Link></p>
+                        <p className="mt-3 space-x-2">Already Have an account? <Link className="text-blue-600 underline" to="/login">Login</Link></p>
                     </form>
-                    <div className="divider text-2xl font-bold">Login With</div>
-                    <div className="text-center">
-                        <button className="text-3xl btn btn-wide bg-transparent hover:bg-orange-100"><FcGoogle/></button>
-                    </div>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Home from "../Pages/Home/Home/Home";
 import Main from "../Layout/Main";
 import Blogs from "../Pages/Blogs/Blogs";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
         {
             path:"/login",
             element:<Login/>
+        },
+        {
+            path:"/signup",
+            element:<SignUp/>
         },
       ]
     },
